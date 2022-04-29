c1b7fbb43df405915306a974faec104df7b4a290
Déric Marchand <dericmarchand@gmail.com>
2022-04-29 15:25:12 -0400

    little work on layout + icons

--- FinalDericMarchand/app/src/main/java/com/dm/tpfinal/Defi.java
+++ FinalDericMarchand/app/src/main/java/com/dm/tpfinal/Defi.java
@@ -1,22 +1,24 @@
 package com.dm.tpfinal;
 
 public class Defi {
+    private String nom;
     private boolean reussi;
     private String description;
     private String activitePhysique;
 
-    public Defi(boolean reussi, String description, String activitePhysique) {
+    public Defi(String nom, String description, boolean reussi, String activitePhysique) {
+        this.nom = nom;
         this.reussi = reussi;
         this.description = description;
         this.activitePhysique = activitePhysique;
     }
 
-    public boolean isReussi() {
-        return reussi;
+    public String getNom() {
+        return nom;
     }
 
-    public void setReussi(boolean reussi) {
-        this.reussi = reussi;
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 
     public String getDescription() {
@@ -25,6 +27,14 @@ public class Defi {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public boolean isReussi() {
+        return reussi;
+    }
+
+    public void setReussi(boolean reussi) {
+        this.reussi = reussi;
     }
 
     public String getActivitePhysique() {
--- FinalDericMarchand/app/src/main/java/com/dm/tpfinal/ListeDefis.java
+++ FinalDericMarchand/app/src/main/java/com/dm/tpfinal/ListeDefis.java
@@ -1,4 +1,24 @@
 package com.dm.tpfinal;
 
-public class ListeDefis {
+import android.content.Context;
+
+import java.io.Serializable;
+import java.util.Vector;
+
+public class ListeDefis implements Serializable {
+    private Vector<Defi> listeDefis;
+    private static ListeDefis instance;
+    private Context context;
+
+    public static ListeDefis getInstance(Context context) {
+        if (instance == null) {
+            instance = new ListeDefis(context.getApplicationContext());
+        }
+        return instance;
+    }
+
+    private ListeDefis(Context context) {
+        this.context = context;
+        this.listeDefis = new Vector<Defi>();
+    }
 }
