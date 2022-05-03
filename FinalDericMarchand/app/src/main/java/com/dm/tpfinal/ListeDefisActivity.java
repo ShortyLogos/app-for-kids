66993cc185ecedfb74668abcff5b75249ba2b69e
Déric Marchand <dericmarchand@gmail.com>
2022-05-03 16:08:21 -0400

    working a bit on QRcode

--- FinalDericMarchand/app/src/main/java/com/dm/tpfinal/ListeDefisActivity.java
+++ FinalDericMarchand/app/src/main/java/com/dm/tpfinal/ListeDefisActivity.java
@@ -1,10 +1,18 @@
 package com.dm.tpfinal;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.Hashtable;
 import java.util.Vector;
@@ -14,6 +22,8 @@ public class ListeDefisActivity extends AppCompatActivity {
     ListView listeItemsDefis;
     Vector<Hashtable<String, Object>> infoDefis;
     ListeDefis listeDefis;
+    Button codeQR;
+    TextView testQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class ListeDefisActivity extends AppCompatActivity {
 
         listeDefis = ListeDefis.getInstance(this);
         infoDefis = listeDefis.getInfoDefis();
+        codeQR = findViewById(R.id.codeQR);
 
         listeItemsDefis = findViewById(R.id.listeItemsDefis);
         int[] conteneurs = {R.id.iconeDefi, R.id.nomDefi, R.id.reussi};
@@ -33,5 +44,31 @@ public class ListeDefisActivity extends AppCompatActivity {
                 conteneurs);
 
         listeItemsDefis.setAdapter(simpleAdapter);
+
+        codeQR.setOnClickListener(v -> {
+            IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+            intentIntegrator.setPrompt("Scan a barcode or QR Code");
+            intentIntegrator.setOrientationLocked(true);
+            intentIntegrator.initiateScan();
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+                messageText.setText(intentResult.getContents());
+                messageFormat.setText(intentResult.getFormatName());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
